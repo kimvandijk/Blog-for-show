138f09acb9d6f6cef8adbea2c73cc5367739dd34
Kim <dijk.kim.van@gmail.com>
2016-12-29 11:15:06 +0100

    4.6 minor changes in controllers

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,15 +1,14 @@
 class CommentsController < ApplicationController
 	
 	http_basic_authenticate_with name: "dhh", password: "secret", only: :destroy
+  before_action :prepare_article
 
 	def create
-    @article = Article.find(params[:article_id])
     @comment = @article.comments.create(comment_params)
     redirect_to article_path(@article)
   end
 
   def destroy
-    @article = Article.find(params[:article_id])
     @comment = @article.comments.find(params[:id])
     @comment.destroy
     redirect_to article_path(@article)
@@ -19,4 +18,8 @@ class CommentsController < ApplicationController
     def comment_params
       params.require(:comment).permit(:commenter, :body)
     end
+
+  def prepare_article
+    @article = Article.find(params[:article_id])  
+  end  
 end
